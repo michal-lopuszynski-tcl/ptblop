1b068b1f2f5759b899dad197bd5ce6dc9fe19f46
Michal Lopuszynski <michal.lopuszynski@tcl.com>
2025-04-09 22:33:39 +0200

    [ptblopgen] Add tests_full

---
+++ ptblopgen/tests_full/test_llm_paretoeval.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -n "Started $(basename $0) "
+
+rm -rf tmp.paretoeval
+mkdir -p tmp.paretoeval
+
+SECONDS=0
+cp data/pareto_front_0004.json tmp.paretoeval
+blop paretoeval \
+   --config data/config.yaml \
+   --pareto-path  tmp.paretoeval/pareto_front_0004.json \
+   > tmp.paretoeval/log 2>&1
+ERRCODE=$?
+DURATION=$SECONDS
+DURATION_STR="$((DURATION / 60)):$((DURATION % 60))"
+
+if [ ${ERRCODE} -ne 0 ]; then
+    echo -e "[${DURATION_STR}] ... ${RED}FAILED${NC}"
+else
+    echo -e "[${DURATION_STR}] ... ${GREEN}PASSED${NC}"
+fi
+
+exit ${ERRCODE}
---
+++ ptblopgen/tests_full/test_llm_sample.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -n "Started $(basename $0) "
+
+rm -rf tmp.sample
+mkdir -p tmp.sample
+
+SECONDS=0
+blop sample \
+  --config data/config.yaml \
+  --output-path tmp.sample \
+  > tmp.sample/log 2>&1
+ERRCODE=$?
+DURATION=$SECONDS
+DURATION_STR="$((DURATION / 60)):$((DURATION % 60))"
+
+
+if [ ${ERRCODE} -ne 0 ]; then
+    echo -e "[${DURATION_STR}] ... ${RED}FAILED${NC}"
+else
+    echo -e "[${DURATION_STR}] ... ${GREEN}PASSED${NC}"
+fi
+
+exit ${ERRCODE}
