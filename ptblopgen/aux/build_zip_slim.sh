b9d8dee4b98926e4e52fcb5c29bca4d0f099e0fc
Michal Lopuszynski <michal.lopuszynski@tcl.com>
2025-05-20 13:13:07 +0200

    [ptblopgen] Improve zip building functionality

---
+++ ptblopgen/aux/build_zip_slim.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    exit 1
+fi
+
+
+ZIP_DIR="${1}/slim"
+TMP_DIR=tmp.$$
+
+rm -rf ${ZIP_DIR}
+
+ZIP_SUFFIX="-mod"
+git diff --exit-code --quiet && ZIP_SUFFIX=""
+ZIP_NAME="ptblopgen-slim-$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)${ZIP_SUFFIX}.zip"
+
+mkdir -p ${ZIP_DIR}/${TMP_DIR}
+cd ${ZIP_DIR}/${TMP_DIR}
+
+python3 -mpip install --no-dependencies ../../../../ptblopgen --target .
+python3 -mpip install --no-dependencies ../../../../ptblopgen_evalplus --target .
+
+printf "import sys\nimport ptblopgen.cli\n\nsys.exit(ptblopgen.cli.main())\n" > __main__.py
+
+find . | cut -c3- | zip -r ../${ZIP_NAME} -@
+
+cd ..
+
+rm -rf ${TMP_DIR}
