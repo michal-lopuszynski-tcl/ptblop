bba7e0e553fe7b894a8c1ad449e9af6eea40bc19
Michal Lopuszynski <michal.lopuszynski@tcl.com>
2025-05-23 22:46:27 +0200

    [ptblopgen_evalplus] Unify naming in the example

--- ptblopgen_evalplus/examples/run_evalplus/run_evalplus.sh
+++ ptblopgen_evalplus/examples/run_evalplus/run_evalplus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf tmp
-TOKENIZERS_PARALLELISM=false python3 runevalplus.py \
+TOKENIZERS_PARALLELISM=false python3 run_evalplus.py \
     --model Qwen/Qwen3-0.6B \
     --dataset humaneval \
     --enable-thinking=False \
