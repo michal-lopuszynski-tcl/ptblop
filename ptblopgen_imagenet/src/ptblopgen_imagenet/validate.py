1a88de019f1bbbc2cce7460c7aab6f93138b0e55
Michal Lopuszynski <michal.lopuszynski@tcl.com>
2025-05-27 21:52:44 +0200

    [ptblopgen_imagenet] 0.0.3 0.0.2 Refactor + clean-up continued

--- ptblopgen_imagenet/src/ptblopgen_imagenet/validate.py
+++ ptblopgen_imagenet/src/ptblopgen_imagenet/validate.py
@@ -49,7 +49,7 @@ from typing import Optional
 
 
 class EvalConfig:
-    data_dir: str
+    dataset: str
     split: str
     num_samples: Optional[int]
     dataset_download: bool
@@ -103,7 +103,6 @@ class EvalConfig:
     retry: bool
 
     def __init__(self):
-        self.data_dir = "/nas/datasets/vision/tenegami/"
         self.dataset = ""
         self.split = "validation"
         self.num_samples = None
@@ -158,136 +157,117 @@ class EvalConfig:
         self.retry = False
 
 
-def validate(model, args):
+def validate(*, model, device, data_dir, config):
     # might as well try to validate something
-    args.pretrained = args.pretrained or not args.checkpoint
-    args.prefetcher = not args.no_prefetcher
+    config.pretrained = config.pretrained or not config.checkpoint
+    config.prefetcher = not config.no_prefetcher
 
     if torch.cuda.is_available():
         torch.backends.cuda.matmul.allow_tf32 = True
         torch.backends.cudnn.benchmark = True
 
-    device = torch.device(args.device)
-
     model_dtype = None
-    if args.model_dtype:
-        assert args.model_dtype in ('float32', 'float16', 'bfloat16')
-        model_dtype = getattr(torch, args.model_dtype)
+    if config.model_dtype:
+        assert config.model_dtype in ('float32', 'float16', 'bfloat16')
+        model_dtype = getattr(torch, config.model_dtype)
 
     # resolve AMP arguments based on PyTorch / Apex availability
     use_amp = None
     amp_autocast = suppress
-    if args.amp:
+    if config.amp:
         assert model_dtype is None or model_dtype == torch.float32, 'float32 model dtype must be used with AMP'
-        if args.amp_impl == 'apex':
+        if config.amp_impl == 'apex':
             assert has_apex, 'AMP impl specified as APEX but APEX is not installed.'
-            assert args.amp_dtype == 'float16'
+            assert config.amp_dtype == 'float16'
             use_amp = 'apex'
             _logger.info('Validating in mixed precision with NVIDIA APEX AMP.')
         else:
-            assert args.amp_dtype in ('float16', 'bfloat16')
+            assert config.amp_dtype in ('float16', 'bfloat16')
             use_amp = 'native'
-            amp_dtype = torch.bfloat16 if args.amp_dtype == 'bfloat16' else torch.float16
+            amp_dtype = torch.bfloat16 if config.amp_dtype == 'bfloat16' else torch.float16
             amp_autocast = partial(torch.autocast, device_type=device.type, dtype=amp_dtype)
             _logger.info('Validating in mixed precision with native PyTorch AMP.')
     else:
         _logger.info(f'Validating in {model_dtype or torch.float32}. AMP not enabled.')
 
-    if args.fuser:
-        set_jit_fuser(args.fuser)
+    if config.fuser:
+        set_jit_fuser(config.fuser)
 
-    if args.fast_norm:
+    if config.fast_norm:
         set_fast_norm()
 
-    # create model
-    in_chans = 3
-    if args.in_chans is not None:
-        in_chans = args.in_chans
-    elif args.input_size is not None:
-        in_chans = args.input_size[0]
-
-    # model = create_model(
-    #     args.model,
-    #     pretrained=args.pretrained,
-    #     num_classes=args.num_classes,
-    #     in_chans=in_chans,
-    #     global_pool=args.gp,
-    #     scriptable=args.torchscript,
-    #     **args.model_kwargs,
-    # )
-    if args.num_classes is None:
+    if config.num_classes is None:
         assert hasattr(model, 'num_classes'), 'Model must have `num_classes` attr if not set on cmd line/config.'
-        args.num_classes = model.num_classes
+        config.num_classes = model.num_classes
 
-    if args.checkpoint:
-        load_checkpoint(model, args.checkpoint, args.use_ema)
+    if config.checkpoint:
+        load_checkpoint(model, config.checkpoint, config.use_ema)
 
-    if args.reparam:
+    if config.reparam:
         model = reparameterize_model(model)
 
     param_count = sum([m.numel() for m in model.parameters()])
-    _logger.info('Model %s created, param count: %d' % (args.model, param_count))
+    _logger.info('Model %s created, param count: %d' % (config.model, param_count))
 
     data_config = resolve_data_config(
-        vars(args),
+        vars(config),
         model=model,
-        use_test_size=not args.use_train_size,
+        use_test_size=not config.use_train_size,
         verbose=True,
     )
     test_time_pool = False
-    if args.test_pool:
+    if config.test_pool:
         model, test_time_pool = apply_test_time_pool(model, data_config)
 
-    # model = model.to(device=device, dtype=model_dtype)  # FIXME move model device & dtype into create_model
-    if args.channels_last:
+    if config.channels_last:
         model = model.to(memory_format=torch.channels_last)
 
-    if args.torchscript:
+    if config.torchscript:
         assert not use_amp == 'apex', 'Cannot use APEX AMP with torchscripted model'
         model = torch.jit.script(model)
-    elif args.torchcompile:
+    elif config.torchcompile:
         assert has_compile, 'A version of torch w/ torch.compile() is required for --compile, possibly a nightly.'
         torch._dynamo.reset()
-        model = torch.compile(model, backend=args.torchcompile, mode=args.torchcompile_mode)
-    elif args.aot_autograd:
+        model = torch.compile(model, backend=config.torchcompile, mode=config.torchcompile_mode)
+    elif config.aot_autograd:
         assert has_functorch, "functorch is needed for --aot-autograd"
         model = memory_efficient_fusion(model)
 
     if use_amp == 'apex':
         model = amp.initialize(model, opt_level='O1')
 
-    if args.num_gpu > 1:
-        model = torch.nn.DataParallel(model, device_ids=list(range(args.num_gpu)))
+    if config.num_gpu > 1:
+        model = torch.nn.DataParallel(model, device_ids=list(range(config.num_gpu)))
 
     criterion = nn.CrossEntropyLoss().to(device)
 
-    root_dir = args.data_dir
-    if args.input_img_mode is None:
+
+    if config.input_img_mode is None:
         input_img_mode = 'RGB' if data_config['input_size'][0] == 3 else 'L'
     else:
-        input_img_mode = args.input_img_mode
+        input_img_mode = config.input_img_mode
     dataset = create_dataset(
-        root=root_dir,
-        name=args.dataset,
-        split=args.split,
-        download=args.dataset_download,
-        load_bytes=args.tf_preprocessing,
-        class_map=args.class_map,
-        num_samples=args.num_samples,
-        input_key=args.input_key,
+        root=data_dir,
+        name=config.dataset,
+        split=config.split,
+        download=config.dataset_download,
+        load_bytes=config.tf_preprocessing,
+        class_map=config.class_map,
+        num_samples=config.num_samples,
+        input_key=config.input_key,
         input_img_mode=input_img_mode,
-        target_key=args.target_key,
-        trust_remote_code=args.dataset_trust_remote_code,
+        target_key=config.target_key,
+        trust_remote_code=config.dataset_trust_remote_code,
     )
 
-    if args.valid_labels:
-        with open(args.valid_labels, 'r') as f:
+    if config.valid_labels:
+        with open(config.valid_labels, 'r') as f:
             valid_labels = [int(line.rstrip()) for line in f]
     else:
         valid_labels = None
 
-    if args.real_labels:
-        real_labels = RealLabelsImagenet(dataset.filenames(basename=True), real_json=args.real_labels)
+    if config.real_labels:
+        real_labels = RealLabelsImagenet(dataset.filenames(basename=True), real_json=config.real_labels)
     else:
         real_labels = None
 
@@ -295,19 +275,19 @@ def validate(model, args):
     loader = create_loader(
         dataset,
         input_size=data_config['input_size'],
-        batch_size=args.batch_size,
-        use_prefetcher=args.prefetcher,
+        batch_size=config.batch_size,
+        use_prefetcher=config.prefetcher,
         interpolation=data_config['interpolation'],
         mean=data_config['mean'],
         std=data_config['std'],
-        num_workers=args.workers,
+        num_workers=config.workers,
         crop_pct=crop_pct,
         crop_mode=data_config['crop_mode'],
-        crop_border_pixels=args.crop_border_pixels,
-        pin_memory=args.pin_mem,
+        crop_border_pixels=config.crop_border_pixels,
+        pin_memory=config.pin_mem,
         device=device,
         img_dtype=model_dtype or torch.float32,
-        tf_preprocessing=args.tf_preprocessing,
+        tf_preprocessing=config.tf_preprocessing,
     )
 
     batch_time = AverageMeter()
@@ -318,18 +298,18 @@ def validate(model, args):
     model.eval()
     with torch.no_grad():
         # warmup, reduce variability of first batch time, especially for comparing torchscript vs non
-        input = torch.randn((args.batch_size,) + tuple(data_config['input_size'])).to(device=device, dtype=model_dtype)
-        if args.channels_last:
+        input = torch.randn((config.batch_size,) + tuple(data_config['input_size'])).to(device=device, dtype=model_dtype)
+        if config.channels_last:
             input = input.contiguous(memory_format=torch.channels_last)
         with amp_autocast():
             model(input)
 
         end = time.time()
         for batch_idx, (input, target) in enumerate(loader):
-            if args.no_prefetcher:
+            if config.no_prefetcher:
                 target = target.to(device=device)
                 input = input.to(device=device, dtype=model_dtype)
-            if args.channels_last:
+            if config.channels_last:
                 input = input.contiguous(memory_format=torch.channels_last)
 
             # compute output
@@ -353,7 +333,7 @@ def validate(model, args):
             batch_time.update(time.time() - end)
             end = time.time()
 
-            if batch_idx % args.log_freq == 0:
+            if batch_idx % config.log_freq == 0:
                 _logger.info(
                     'Test: [{0:>4d}/{1}]  '
                     'Time: {batch_time.val:.3f}s ({batch_time.avg:.3f}s, {rate_avg:>7.2f}/s)  '
@@ -376,7 +356,7 @@ def validate(model, args):
     else:
         top1a, top5a = top1.avg, top5.avg
     results = OrderedDict(
-        model=args.model,
+        model=config.model,
         top1=round(top1a, 4), top1_err=round(100 - top1a, 4),
         top5=round(top5a, 4), top5_err=round(100 - top5a, 4),
         param_count=round(param_count / 1e6, 2),
@@ -532,12 +512,13 @@ class ImageNetEvaluator:
 def main():
     setup_default_logging()
     config = EvalConfig()
-    config.data_dir = "/home/lopusz/Datasets/datahub/vision/imagenet-v2"
+    data_dir = "/home/lopusz/Datasets/datahub/vision/imagenet-v2"
     config.model = "mobilevitv2_200.cvnets_in22k_ft_in1k"
     config.batch_size = 64
     model = timm.create_model(config.model, pretrained=True)
-    model.to(torch.device("cuda"))
-    validate(model, config)
+    device = torch.device("cuda")
+    model.to(device)
+    validate(model=model, device=device, data_dir=data_dir, config=config)
 
 
 if __name__ == "__main__":
